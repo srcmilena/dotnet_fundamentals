b11a16c92ee74b6c7455652a15ee6fb9877b4e43
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-06-07 12:47:16 -0300

    novo projeto

--- DigitalInnovationOne/Program.cs
+++ DigitalInnovationOne/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace DigitalInnovationOne // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem-vinda ao curso de .NET {i}");
+            }
+        }
+    }
+}
