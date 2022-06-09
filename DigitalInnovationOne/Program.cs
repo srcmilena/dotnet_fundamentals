5940d97b267bc4672502b267aea19916037ea86d
srcmilena <87768754+srcmilena@users.noreply.github.com>
2022-06-08 22:37:27 -0300

    novos exercicios

--- DigitalInnovationOne/Program.cs
+++ DigitalInnovationOne/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-using System;
+/*using System;
 
 namespace DigitalInnovationOne // Note: actual namespace depends on the project name.
 {
@@ -15,6 +15,64 @@ namespace DigitalInnovationOne // Note: actual namespace depends on the project 
             {
                 Console.WriteLine($"Bem-vinda ao curso de .NET {i}");
             }
+        }
+    }
+}
+*/
+/*using System;
+
+namespace DigitalInnovationOne // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool nome = true;
+
+            if (nome == true) {
+                Console.WriteLine("Milena");
+            }
+        }
+    }
+}*/
+
+using System;
+
+namespace DigitalInnovationOne // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++) {
+                Console.WriteLine("Digite um número: ");
+                int num = Console.ReadLine();
+
+                for (num = 0; i <= 10; i++) {
+                    if ()
+                }
+            } 
+            /*Console.WriteLine("Digite o número 1:");
+            int num1 = Console.ReadLine();
+            Console.WriteLine("Digite o número 2:");
+            int num2 = Console.ReadLine();
+            Console.WriteLine("Digite o número 3:");
+            int num3 = Console.ReadLine();
+            Console.WriteLine("Digite o número 4:");
+            int num4 = Console.ReadLine();
+            Console.WriteLine("Digite o número 5:");
+            int num5 = Console.ReadLine();
+            Console.WriteLine("Digite o número 6:");
+            int num6 = Console.ReadLine();
+            Console.WriteLine("Digite o número 7:");
+            int num7 = Console.ReadLine();
+            Console.WriteLine("Digite o número 8:");
+            int num8 = Console.ReadLine();
+            Console.WriteLine("Digite o número 9:");
+            int num9 = Console.ReadLine();
+            Console.WriteLine("Digite o número 10:");
+            int num10 = Console.ReadLine();*/
+
         }
     }
 }
